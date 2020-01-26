5b91f03acd8d0e42924f47095c4f893043af3123
cincottash <cincotta@udel.edu>
2020-01-26 16:03:52 -0500

    Getting there

---
+++ galaxy.py
@@ -0,0 +1,6 @@
+class Galaxy():
+	def __init__(self, pos, velocity, radius, numStars):
+		self.pos = pos
+		self.velocity = velocity
+		self.radius = radius
+		self.numStars = numStars
---
+++ main.py
@@ -0,0 +1,11 @@
+from vpython import *
+from galaxy import *
+
+def main():
+	x = sphere(pos=vector(0,0,0), radius = 10)
+	#milkyway = Galaxy((0,0,0), 0, 10, 0)
+
+
+
+if __name__ == '__main__':
+	main()
